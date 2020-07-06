a5928b15c224e94cf6f18216827c6255f46fef40
HongNyungKi <hnk2017@naver.com>
2020-07-06 19:52:19 +0900

    [add]props

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import Hello from './Hello';
-import './App.css'
 
 function App() {
-  const name = 'Hong';
-  const style = {
-    backgroundColor:'black',
-    color:'red',
-    fontSize:'24',
-    padding:'1rem'
-  };
 
   return (
     <>
-      <Hello />
-      <div>안녕히계세요.</div>
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
+      <Hello name="react"/>
+      <Hello color="red"/>
     </>
   )
 }
