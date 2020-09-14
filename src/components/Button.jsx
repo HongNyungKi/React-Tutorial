2e0306d7e6770a31ba67a52c8d146a35e8cde345
HongNyungKi <hnk2017@naver.com>
2020-09-14 14:05:51 +0900

    add

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Button.scss';
+import classNames from 'classnames';
+
+function Button({ children, size }) {
+    return (
+        <>
+            <button className={classNames('Button', size)}>{children}</button>
+        </>
+    )
+}
+
+Button.defaultProps = {
+    size: 'medium'
+}
+
+export default Button;
+
